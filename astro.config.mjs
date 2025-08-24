be99c2b590794b3564f5121f34e876e8d2b05f3b
mosidas <sspydery0@gmail.com>
2025-08-24 14:54:13 +0900

    mod

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
 // @ts-check
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://mosidas.github.io",
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
