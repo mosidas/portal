a15f101ea599cabe28d02bd77a5e1451a65f32d9
yamaguchi shota <41434682+mosidas@users.noreply.github.com>
2025-08-24 21:31:07 +0900

    feat: #3 vercel対応 (#4)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,18 @@
 // @ts-check
-
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://mosidas.github.io",
   integrations: [icon()],
-  
+
   markdown: {
     shikiConfig: {
-      theme: 'github-light',
+      theme: "github-light",
       themes: {
-        light: 'github-light',
-        dark: 'github-dark',
+        light: "github-light",
+        dark: "github-dark",
       },
     },
   },
